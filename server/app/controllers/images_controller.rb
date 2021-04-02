7a978315ad73b9493378a13f335577c55a565124
Julienne San Luis <julienne.codes@gmail.com>
2021-04-02 02:30:18 -0400

    cleaned up code

--- server/app/controllers/images_controller.rb
+++ server/app/controllers/images_controller.rb
@@ -9,20 +9,12 @@ class ImagesController < ApplicationController
         new_image_name = params[:newImageName]
         new_image_url = params[:newImageUrl]
         new_image = Image.create(name: new_image_name, url: new_image_url)
-        # render json: {new_image: new_image}
-        # can I send it like this {new_image}
-        # render json: {message: "New Image Created"}
         render json: {:message => "New Image Created"}
     end
     # handles post requests to /images
 
     def show
         id = params[:id]
-        # does this automatically get the id from the url
-        # I'm not sure if I have to post the information from react
-        # or if the interpolated url will give enough information
-        # how does rails know params refers to the url
-        # doesn't params sometimes refer to the information posted in a post request?
         current_image = Image.find_by(id: id)
         render json: {image: current_image}
     end
