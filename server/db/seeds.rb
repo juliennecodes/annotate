098daa96235db9363c2f80cb00f2aee296022400
Julienne San Luis <julienne.codes@gmail.com>
2021-04-02 19:52:33 -0400

    added data in seeds.rb

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do |i|
+    image = Image.create(name: "Bear", url: "https://i.pinimg.com/564x/23/75/bd/2375bdd087e7dbec4df67e7e3584a1d8.jpg")
+    image.annotations.create(body: "Whimmy-wham-wham-wazzle!")
+    image.annotations.create(body: "Welcome, my friends, to the wondrous world of whimsy that we like to call \"Slurms Centralised Industrial Fabrication Unit\"")
+    image.annotations.create(body: "Why, those are the Grunka Lunkas. They work here in the Slurm factory.")
+end
+
+3.times do |i|
+    image = Image.create(name: "Bear", url: "https://i.pinimg.com/564x/f5/31/33/f531331d7e1f9df020ad7758aa62a51b.jpg")
+    image.annotations.create(body: "Whimmy-wham-wham-wazzle!")
+    image.annotations.create(body: "Welcome, my friends, to the wondrous world of whimsy that we like to call \"Slurms Centralised Industrial Fabrication Unit\"")
+    image.annotations.create(body: "Why, those are the Grunka Lunkas. They work here in the Slurm factory.")
+end
+
+3.times do |i|
+    image = Image.create(name: "Bear", url: "https://i.pinimg.com/564x/26/3c/39/263c390ca20c23f4fe49b282c6a52ad4.jpg")
+    image.annotations.create(body: "Whimmy-wham-wham-wazzle!")
+    image.annotations.create(body: "Welcome, my friends, to the wondrous world of whimsy that we like to call \"Slurms Centralised Industrial Fabrication Unit\"")
+    image.annotations.create(body: "Why, those are the Grunka Lunkas. They work here in the Slurm factory.")
+end
