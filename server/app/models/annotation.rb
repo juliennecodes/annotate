ffd97e730efdf4a425c8bafa57cf6e8e6dad5021
Julienne San Luis <julienne.codes@gmail.com>
2021-04-11 16:38:47 -0400

    added active record validation for annotation

--- server/app/models/annotation.rb
+++ server/app/models/annotation.rb
@@ -1,3 +1,6 @@
 class Annotation < ApplicationRecord
     belongs_to :image
+    validates :visual, presence: true
+    validates :written, presence: true
+    validates :image_id, presence: true
 end
