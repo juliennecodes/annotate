916c2019a95a8f491c225f3afefe98677065873b
Julienne San Luis <julienne.codes@gmail.com>
2021-04-14 13:39:02 -0400

    styled image page - viewing image

--- client/src/components/ImagePage.js
+++ client/src/components/ImagePage.js
@@ -5,7 +5,8 @@ import { Loading } from "./Loading";
 import { Annotations } from "./Annotations";
 import { Annotate } from "./Annotate";
 // import { DeleteImageButton } from "./DeleteImageButton";
-import {FeatureImage} from "./FeatureImage";
+import { FeatureImage } from "./FeatureImage";
+import { Header } from "./Header";
 
 export function ImagePage() {
   const [state, setState] = useState("viewing image");
@@ -18,54 +19,99 @@ export function ImagePage() {
       .then((serverResponse) => setImage(serverResponse.image));
   }, [id]);
 
+  const ViewAnnotationsButton = () => {
+    return (
+      <button
+        className="view-annotations-button"
+        onClick={() => setState("viewing annotations")}
+      >
+        View Annotations
+      </button>
+    );
+  };
+
+  const CloseViewAnnotationsButton = () => {
+    return (
+      <button
+        className="close-view-annotations"
+        onClick={() => setState("viewing image")}
+      >
+        Close view annotations
+      </button>
+    );
+  };
+
+  const AnnotateButton = () => {
+    return (
+      <button
+        className="annotate-button"
+        onClick={() => setState("annotate mode")}
+      >
+        Annotate
+      </button>
+    );
+  };
+
+  const CloseAnnotateButton = () => {
+    return (
+      <button
+        className="close-annotate"
+        onClick={() => setState("viewing image")}
+      >
+        Close annotate
+      </button>
+    );
+  };
+
   const ViewingImage = () => {
     return (
-      <div className="image-page a">
-        <FeatureImage image={image}/>
-        <button className="view-annotations-button" onClick={() => setState("viewing annotations")}>View Annotations</button>
-        <button className="annotate-button" onClick={() => setState("annotate mode")}>Annotate</button>
-      </div>
+      <>
+        <Header />
+        <main className="main">
+          <div className="image-page image-page-view-image">
+            <FeatureImage image={image} />
+            <ViewAnnotationsButton />
+            <AnnotateButton />
+          </div>
+        </main>
+      </>
     );
   };
 
   const ViewingAnnotations = () => {
     return (
-      <div className="image-page b">
-        <FeatureImage image={image}/>
-        <Annotations image={image} />
-        <button
-          className="close-view-annotations"
-          onClick={() => setState("viewing image")}
-        >
-          Close view annotations
-        </button>
-      </div>
+      <>
+        <Header />
+        <main className="main">
+          <div className="image-page image-page-view-annotations">
+            <FeatureImage image={image} />
+            <Annotations image={image} />
+            <CloseViewAnnotationsButton />
+          </div>
+        </main>
+      </>
     );
   };
 
   const AnnotateMode = () => {
     return (
-      <div className="image-page c">
-        <FeatureImage image={image}/>
-        <Annotate image={image} />
-        <button
-          className="close-annotate"
-          onClick={() => setState("viewing image")}
-        >
-          Close annotate
-        </button>
-      </div>
+      <>
+        <Header />
+        <main className="main">
+          <div className="image-page image-page-annotate">
+            <FeatureImage image={image} />
+            <Annotate image={image} />
+            <CloseAnnotateButton />
+          </div>
+        </main>
+      </>
     );
   };
   return image ? (
     <>
-      {state === "viewing image" ? (
-        <ViewingImage />
-      ) : state === "viewing annotations" ? (
-        <ViewingAnnotations />
-      ) : (
-        <AnnotateMode />
-      )}
+      {state === "viewing image" && <ViewingImage />}
+      {state === "viewing annotations" && <ViewingAnnotations />}
+      {state === "annotate mode" && <AnnotateMode />}
     </>
   ) : (
     <Loading />
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,5 +1,4 @@
 import { NavLink } from "react-router-dom";
-import "./Navigation.css";
 
 export function Navigation() {
   return (
