602039c7b1e401f8e85a45fde25d9d7a479ed123
Julienne San Luis <julienne.codes@gmail.com>
2021-04-16 14:02:07 -0400

    reinstated image url validator

--- server/app/models/image.rb
+++ server/app/models/image.rb
@@ -2,5 +2,5 @@ class Image < ApplicationRecord
     has_many :annotations
     validates :name, presence: true
     validates :url, presence: true
-    # validates :url, format: { with: /^https.*(jpg|jpeg|png|webp)$/, :multiline => true, message: "only allows url addresses of images"}
+    validates :url, format: { with: /^https.*(jpg|jpeg|png|webp)$/, :multiline => true, message: "only allows url addresses of images"}
 end
