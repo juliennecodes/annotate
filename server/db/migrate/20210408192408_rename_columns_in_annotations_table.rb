20f739e2c0a5adf2c9659ba2668e2206f2887b0b
Julienne San Luis <julienne.codes@gmail.com>
2021-04-17 16:10:56 -0400

    updated files so app can run locally - deleted migrations file, updated seeds file, reverted to an older version of gemfile

--- server/db/migrate/20210408192408_rename_columns_in_annotations_table.rb
+++
@@ -1,6 +0,0 @@
-class RenameColumnsInAnnotationsTable < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :annotations, :text, :written
-    rename_column :annotations, :image, :visual
-  end
-end
